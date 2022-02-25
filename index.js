ce7390f914f9eaedb4a3a05185a820119484526e
Tamara Salvatori <Tamara.Salvatori-ext@ab-inbev.com>
2022-02-24 21:35:15 -0300

    Add app

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log('Tamara Salvatori');
 
-app.listen(process.env.PORT || 3000);
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Tamara Salvatori")
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Listening on port " + PORT);
+});
